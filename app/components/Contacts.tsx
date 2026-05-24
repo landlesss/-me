0d4958ea40084369f36e24fccedd75aaf661c9a8
1andau <alaskalaska18@yandex.ru>
2026-05-24 21:22:21 +0300

    chore: update linkedin url

--- app/components/Contacts.tsx
+++ app/components/Contacts.tsx
@@ -2,7 +2,7 @@ import s from "./Contacts.module.scss";
 
 const LINKS = [
   { label: "Email",    href: "mailto:alaskalaska22@gmail.com", text: "alaskalaska22@gmail.com" },
-  { label: "LinkedIn", href: "https://linkedin.com",     text: "linkedin.com/in/yourname" },
+  { label: "LinkedIn", href: "https://www.linkedin.com/in/sara-landau-19a797250/", text: "linkedin.com/in/sara-landau" },
   { label: "GitHub",   href: "https://github.com",       text: "github.com/landlesss" },
   { label: "CV",       href: "/cv.pdf",                  text: "Download PDF", download: true },
 ];
